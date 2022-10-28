aa266afb56a763fade40ce9b2b91341b5d19b4cd
asiaphone12 <arkastyle12@gmail.com>
2022-10-28 22:58:35 +0700

    first commit

---
+++ Player.gd
@@ -0,0 +1,75 @@
+extends Area2D
+signal hit
+signal dragsignal
+
+export var speed = 400
+var screen_size
+var dragging = false
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+	connect("dragsignal", self, "_set_drag_pc")
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1	 
+	if Input.is_action_pressed("click") or dragging:
+		var mousepos = get_viewport().get_mouse_position()
+		position = Vector2(mousepos.x, mousepos.y)
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = 'walk'
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = 'up'
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+func _set_drag_pc():
+	dragging =!dragging
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	show()
+	$CollisionShape2D.disabled = false
+
+func _on_KinematicBody2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			emit_signal("dragsignal")
+		elif event.button_index == BUTTON_LEFT and !event.pressed:
+			emit_signal("dragsignal")
+	elif event is InputEventScreenTouch:
+		if event.pressed and event.get_index() == 0:
+			self.position = event.get_position()
+
+
+func _on_Player_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			emit_signal("dragsignal")
+		elif event.button_index == BUTTON_LEFT and !event.pressed:
+			emit_signal("dragsignal")
+	elif event is InputEventScreenTouch:
+		if event.pressed and event.get_index() == 0:
+			self.position = event.get_position()
