4d0eb54d5c5ca62c5bf771d6893210ecf7e75521
celestialaxis <axiscelestial@gmail.com>
2022-10-29 12:51:17 +0700

    Testing After Invited
    If it works there's a node 'Contributors' that attached to a script. Try read the comment.

---
+++ Testing After Invited/contributors.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+"""
+Say 'hi' in this script
+by adding print("hello from celestialaxis")
+in ready function.
+"""
+
+
+func _ready() -> void:
+	print("hello from celestialaxis");
