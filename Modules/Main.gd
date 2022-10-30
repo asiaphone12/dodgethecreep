a8ac882f1c17d62846069618edd76ed6a6ee105c
celestialaxis <axiscelestial@gmail.com>
2022-10-30 09:02:28 +0700

    Fixing Rotation Mob & Structure Folder
    I use tree structure of folder and modules to make it scalable. And fixing the mob rotation.

--- Modules/Main.gd
+++ Modules/Main.gd
@@ -30,7 +30,7 @@ func _on_ScoreTimer_timeout():
 	$HUD.update_score(score)
 
 func _on_MobTimer_timeout():
-	var mob = mob_scene.instance()
+	var mob: RigidBody2D = mob_scene.instance()
 	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
 	mob_spawn_location.offset = randi()
 	var direction = mob_spawn_location.rotation + PI / 2
@@ -38,4 +38,5 @@ func _on_MobTimer_timeout():
 	direction += rand_range(-PI / 4, PI / 4)
 	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
+	mob.rotation = direction;
 	add_child(mob)
---
+++ Script/Nizar.gd
@@ -0,0 +1,10 @@
+extends Node
+
+"""
+Haloo test bisa apa gak?
+
+@celestialaxis
+Clear gan..
+
+test lagi tapi pake github desktop -Nizar
+"""
