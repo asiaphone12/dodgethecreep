88769a77aa6eff85c2fa3dd5608f21176ed126c0
celestialaxis <axiscelestial@gmail.com>
2022-10-30 17:21:11 +0700

    Added free beam if out of screen
    Use visibilitynotifier2d node signal if the beam exit screen.

--- Modules/Projectile File/LaserBeam.gd
+++ Modules/Projectile File/LaserBeam.gd
@@ -13,3 +13,7 @@ func _process(delta):
 func _on_LaserBeam_body_entered(body: RigidBody2D):
 	body.queue_free();
 	self.queue_free();
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	self.queue_free();
